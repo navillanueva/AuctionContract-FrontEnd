ce0980b513aabca9d76df90d2cf9bec5ee35288c
navillanueva <nicolasarnedo99@gmail.com>
2023-01-21 21:40:47 +0100

    header

---
+++ components/ManualHeader.jsx
@@ -0,0 +1,22 @@
+import { useMoralis } from "react-moralis"
+
+export default function ManualHeader() {
+    const { enableWeb3, account } = useMoralis()
+    return (
+        <div>
+            {account ? (
+                <div>
+                    Connected to {account.slice(0, 6)}...{account.slice(account.length - 4)}
+                </div>
+            ) : (
+                <button
+                    onClick={async () => {
+                        await enableWeb3()
+                    }}
+                >
+                    Connect
+                </button>
+            )}
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,11 @@
 import Head from "next/head"
-import Image from "next/image"
-import { Inter } from "@next/font/google"
-import styles from "@/styles/Home.module.css"
-import Header from "../components/Header"
 
-const inter = Inter({ subsets: ["latin"] })
+import ManualHeader from "../components/ManualHeader"
 
 export default function Home() {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>Auction Website</title>
-                <meta name="description" content="Website for auctioning my stuff" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Header></Header>
-            Hellooo
+        <div>
+            <ManualHeader>Hellooo</ManualHeader>
         </div>
     )
 }
